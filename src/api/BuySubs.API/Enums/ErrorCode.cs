f5eae40fb2653ee341fcf40980e04693fb3d756a
Maks.Szokalski <42069493+illunix@users.noreply.github.com>
2022-08-25 01:39:46 +0200

    update

---
+++ src/api/BuySubs.API/Enums/ErrorCode.cs
@@ -0,0 +1,11 @@
+﻿namespace BuySubs.API.Enums;
+
+public enum ErrorCode
+{
+    General = 1,
+    NotFound,
+    InvalidCredentials,
+    InvalidToken,
+    ExpiredRefreshToken,
+    UserWithThisEmailAlreadyExist
+}
---
+++ src/api/BuySubs.API/Extensions/ExceptionFilterExtensions.cs
@@ -0,0 +1,35 @@
+﻿using BuySubs.API.Enums;
+using BuySubs.BLL.Exceptions;
+using BuySubs.BLL.Exceptions.Auth;
+using FluentValidation;
+using System.Net;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal static class ExceptionFilterExtensions
+{
+    public static (
+        HttpStatusCode statusCode,
+        ErrorCode errorCode
+    ) ParseException(this Exception exception)
+    {
+        return exception switch
+        {
+            NotFoundException _ => (
+                HttpStatusCode.NotFound,
+                ErrorCode.NotFound
+            ),
+            InvalidCredentialsException _ => (
+                HttpStatusCode.Unauthorized, 
+                ErrorCode.InvalidCredentials
+            ),
+            UserWithThisEmailAlreadyExistException _ => (
+                HttpStatusCode.Conflict, 
+                ErrorCode.UserWithThisEmailAlreadyExist
+            ),
+            _ => (
+                HttpStatusCode.InternalServerError,
+                ErrorCode.General
+            ),
+        };
+    }
+}
