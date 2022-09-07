e75b79b6d0cb5dd05201d9ea2f84fe8ab8a611f9
Maks.Szokalski <42069493+illunix@users.noreply.github.com>
2022-09-07 07:25:02 +0200

    Squashed commit of the following:
    commit dd492dd8020f23dcf81aebfc75fbb1a08c86c116
    Author: Maks.Szokalski <42069493+illunix@users.noreply.github.com>
    Date:   Wed Sep 7 07:05:14 2022 +0200
    
        update
    
    commit 2d5328308d1a9bbf95a462128f60d9e2863c6b72
    Author: Maks.Szokalski <42069493+illunix@users.noreply.github.com>
    Date:   Wed Sep 7 06:36:32 2022 +0200
    
        update
    
    commit 1abdde32b4ec73bea78ec0465358a810f3479bff
    Author: Maks.Szokalski <42069493+illunix@users.noreply.github.com>
    Date:   Wed Sep 7 04:57:18 2022 +0200
    
        update
    
    commit 41d7198f550eb3f2e4f34bf9908f658b375b29a1
    Author: Maks.Szokalski <42069493+illunix@users.noreply.github.com>
    Date:   Wed Sep 7 04:13:27 2022 +0200
    
        update
    
    commit 4b5e34e075eb69d7084d8f65721630ff9e7b753d
    Author: Maks.Szokalski <42069493+illunix@users.noreply.github.com>
    Date:   Wed Sep 7 04:13:23 2022 +0200
    
        update gitignore

---
+++ infrastructure/resources/rds/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+module "vpc" {
+  source = "../vpc"
+}
+
+resource "aws_rds_cluster" "main" {
+  serverlessv2_scaling_configuration {
+    min_capacity = 2
+    max_capacity = 4
+  }
+
+  engine                  = "aurora-postgresql"
+  availability_zones      = ["eu-west-2a"]
+  database_name           = "BuySubs"
+  master_username         = "foo"
+  master_password         = "baregeogoevne"
+  vpc_security_group_ids  = [module.vpc.aws_security_group_allow_all_id]
+  db_subnet_group_name    = aws_db_subnet_group.main.id
+  skip_final_snapshot     = true
+  backup_retention_period = 0
+  apply_immediately       = true
+}
+
+resource "aws_rds_cluster_instance" "main" {
+  count                        = 1
+  cluster_identifier           = aws_rds_cluster.main.id
+  instance_class               = "db.serverless"
+  engine                       = aws_rds_cluster.main.engine
+  engine_version               = aws_rds_cluster.main.engine_version
+  publicly_accessible          = true
+  db_subnet_group_name         = aws_rds_cluster.main.db_subnet_group_name
+  performance_insights_enabled = false
+}
+
+resource "aws_db_subnet_group" "main" {
+  subnet_ids = [module.vpc.aws_subnet_main_id, module.vpc.aws_subnet_second_id]
+}
