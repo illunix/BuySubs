3fe056f566d7bcada3e7a0707a706d7d746985a5
Maks.Szokalski <42069493+illunix@users.noreply.github.com>
2022-09-08 14:30:04 +0200

    update

--- infrastructure/resources/ec2/main.tf
+++ infrastructure/resources/ec2/main.tf
@@ -18,7 +18,8 @@ resource "aws_ec2_client_vpn_endpoint" "vpn" {
   client_cidr_block = "10.20.0.0/22"
   split_tunnel = true
   server_certificate_arn = module.acm.aws_acm_certificate_validation_vpn_server_arn
-
+  security_group_ids = [module.vpc.aws_security_group_vpn_access_id]
+  
   authentication_options {
     type = "certificate-authentication"
     root_certificate_chain_arn = module.acm.aws_acm_certificate_vpn_client_root_arn
@@ -30,10 +31,9 @@ resource "aws_ec2_client_vpn_endpoint" "vpn" {
 }
 
 resource "aws_ec2_client_vpn_network_association" "vpn_subnets" {
-  count = length(aws_subnet.sn_az)
+  count = 1
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.vpn.id
-  subnet_id = aws_subnet.sn_az[count.index].id
-  security_groups = [aws_security_group.vpn_access.id]
+  subnet_id = module.vpc.aws_subnet_main_id
 
   lifecycle {
     ignore_changes = [subnet_id]
@@ -42,6 +42,6 @@ resource "aws_ec2_client_vpn_network_association" "vpn_subnets" {
 
 resource "aws_ec2_client_vpn_authorization_rule" "vpn_auth_rule" {
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.vpn.id
-  target_network_cidr = aws_vpc.main.cidr_block
+  target_network_cidr = module.vpc.aws_vpc_cidr_block
   authorize_all_groups = true
 }
--- infrastructure/resources/vpc/outputs.tf
+++ infrastructure/resources/vpc/outputs.tf
@@ -2,6 +2,10 @@ output "aws_security_group_allow_all_id" {
   value = aws_security_group.allow_all.id
 }
 
+output "aws_security_group_vpn_access_id" {
+  value = aws_security_group.vpn_access.id
+}
+
 output "aws_subnet_main_id" {
   value = aws_subnet.main.id
 }
@@ -12,4 +16,8 @@ output "aws_subnet_second_id" {
 
 output "aws_route_table_id" {
   value = aws_route_table.main.id
+}
+
+output "aws_vpc_cidr_block" {
+  value = aws_vpc.main.cidr_block
 }
